88de68eea7b71845e7facb836941bfa85fdbc8a4
Noemi souza <noemigiseleee@gmail.com>
2023-09-04 19:30:37 -0300

    Req- 1

--- src/componentes/Login/login.tsx
+++ src/componentes/Login/login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../../services/userAPI';
 import { MsgLoading } from '../msgloading/msgloading';
